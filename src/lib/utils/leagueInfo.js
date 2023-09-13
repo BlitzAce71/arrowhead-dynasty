1b83bc4fcc0078957548526769a754585e91349f
BlitzAce71 <32716099+BlitzAce71@users.noreply.github.com>
2023-09-13 17:10:25 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to the Arrowhead Dynasty league!</p>
+  <p>                     Welcome to the Arrowhead Dynasty league!</p>
 `;
 
 /*   STEP 3   */
